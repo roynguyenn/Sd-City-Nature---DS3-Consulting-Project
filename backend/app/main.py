ee4c473591eeef8567cfa0d32f21619e939ec5c4
roynguyenn <roycenguyen1111@gmail.com>
2026-03-17 20:28:41 -0700

    vercel link

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,6 +25,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://sd-city-nature-ds-3-consulting-project.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
